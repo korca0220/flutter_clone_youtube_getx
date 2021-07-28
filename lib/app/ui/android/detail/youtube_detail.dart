4bf397c59891063a395f5a17a5523fc1f3c088ab
Junewoo <junewoo@cavuai.com>
2021-07-28 16:09:00 +0900

    detail page ui 추가

--- lib/app/ui/android/detail/youtube_detail.dart
+++ lib/app/ui/android/detail/youtube_detail.dart
@@ -1,14 +1,152 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
 class YoutubeDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          Get.parameters["videoId"].toString(),
-        ),
+      appBar: AppBar(
+        elevation: 0.0,
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 250,
+            color: Colors.grey.withOpacity(0.5),
+          ),
+          Expanded(
+            child: _description(),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _description() {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          _titleZone(),
+          Divider(height: 1),
+          _descriptionZone(),
+          _bottomZone(),
+          SizedBox(height: 20),
+          Divider(height: 1),
+          _ownerZone()
+        ],
+      ),
+    );
+  }
+
+  Widget _titleZone() {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            '개발하는 남자 유튜브 영상 다시보기',
+            style: TextStyle(
+              fontSize: 15,
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              Text(
+                '조회수 100회',
+                style: TextStyle(
+                  fontSize: 13,
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
+              Text(" · "),
+              Text(
+                '2021-02-03',
+                style: TextStyle(
+                  fontSize: 13,
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _descriptionZone() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+      child: Text(
+        "안녕하세요? 저는 저입니다",
+        style: TextStyle(fontSize: 14),
+      ),
+    );
+  }
+
+  Widget _bottomOne(String iconPath, String text) {
+    return Column(
+      children: [
+        SvgPicture.asset("assets/svg/icons/$iconPath.svg"),
+        Text(text),
+      ],
+    );
+  }
+
+  Widget _bottomZone() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        _bottomOne("like", "1000"),
+        _bottomOne("disklike", "0"),
+        _bottomOne("share", "공유"),
+        _bottomOne("save", "저장"),
+      ],
+    );
+  }
+
+  Widget _ownerZone() {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 25,
+            backgroundColor: Colors.grey.withOpacity(0.5),
+            backgroundImage: Image.network(
+              "https://yt3.ggpht.com/yti/APfAmoG9Z6GyerJlMhoi1lnfy7GBQl6oCeSLL3CfDg=s88-c-k-c0x00ffffff-no-rj-mo",
+              fit: BoxFit.cover,
+            ).image,
+          ),
+          SizedBox(width: 15),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  "Junewoo park",
+                  style: TextStyle(fontSize: 18),
+                ),
+                Text(
+                  "구독자 1000",
+                  style: TextStyle(
+                      fontSize: 14, color: Colors.black.withOpacity(0.6)),
+                ),
+              ],
+            ),
+          ),
+          GestureDetector(
+            child: Text(
+              "구독",
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.black.withOpacity(0.6),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
